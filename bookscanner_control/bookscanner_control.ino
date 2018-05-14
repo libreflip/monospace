cbf38cc00ee534108849f043f6d5e1a332416de5
SEGFAULT <fakespamtrap@gmail.com>
2018-05-14 20:47:47 +0200

    hacky temp serial

--- bookscanner_control/bookscanner_control.ino
+++ bookscanner_control/bookscanner_control.ino
@@ -1,11 +1,33 @@
+#define DEBUG
 #include "bookscanner.h"
 
+Bookscanner b = Bookscanner();
+
 void setup() {
-  // put your setup code here, to run once:
-  Bookscanner b = Bookscanner();
+    // put your setup code here, to run once:
+    Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    char cmd;
+    if (Serial.available() > 0) {
+        // read the incoming byte:
+        cmd = Serial.read();
+        switch(cmd) {
+        case 'u':
+             Serial.print("UP\n");
+             b.raise_box();
+             break;
+        case 'd':
+             Serial.print("DOWN\n");
+             b.lower_box();
+             break;
+        case 'f':
+             Serial.print("FLIP\n");
+             // fuk it I am hardcoding it, 
+             // rebuild it if you wanna change it
+             b.flip_page(100);
+             break;
+        }
+    }
 }

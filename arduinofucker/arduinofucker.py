0a00ac069768d3f35e67e64e60d571b519d8d2fa
SEGFAULT <fakespamtrap@gmail.com>
2019-07-07 00:56:08 +0200

    put AF Serial Rx on its own thread so that we can listen to the debug log in real time.

--- arduinofucker/arduinofucker.py
+++ arduinofucker/arduinofucker.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 from serial import Serial
+import threading
 import sys
+import time
+
+running = threading.Event()
+running.set()
+
+def thread_read(ser):
+    while running.is_set():
+        buf = ser.readline(1024)
+        if buf != b'':
+            print(buf)
+
 
 port = sys.argv[1]
 
-s = Serial('/dev/'+port, 9600, timeout=1)
+s = Serial(port, 9600, timeout=1)
+
+th = threading.Thread(target=thread_read, args=(s, )) # I both love and hate python for this.
+th.start()
 
 while True:
     print("Menu:")
@@ -19,25 +34,18 @@ while True:
     c = int(input())
     if c == 1:
         s.write(b'\x02\x01')
-        back = s.read(100)
-        print(back)
     if c == 2:
         s.write(b'\x02\x00')
-        back = s.read(100)
-        print(back)
     if c == 3:
         s.write(b'\x04\x01')
-        back = s.read(100)
-        print(back)
     if c == 4:
         s.write(b'\x04\x00') #C-C++ Reverenz 2nd edition
-        back = s.read(100)
-        print(back)
     if c == 5:
         s.write(b'\x08\x13')
-        back = s.read(800)
-        print(back)
     if c == 6:
+        running.clear()
+        th.join()
         break
+    time.sleep(2);
 
 s.close()
